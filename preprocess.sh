dcf2f922469ea32e28fbcdb2b45c828f366482a5
ZzeongB <parkjeong02@snu.ac.kr>
2024-04-10 17:45:01 +0900

    running code

--- preprocess.sh
+++ preprocess.sh
@@ -7,8 +7,8 @@ API_KEY=$1
 for FILE_PATH in $INPUT_PATH*.csv
 do
     FILE=$(basename $FILE_PATH .csv)
-    python preprocessing/instruction_generator.py --input_path $FILE_PATH --save_path $SAVE_PATH/${FILE}_instruction.jsonl --api_key $API_KEY
-    python preprocessing/postproc_question.py --input_path $FILE_PATH --save_path $SAVE_PATH/${FILE}_question.jsonl
-    python preprocessing/answer_generator.py --input_path $FILE_PATH --save_path $SAVE_PATH/${FILE}_answer.jsonl --api_key $API_KEY
-    python preprocessing/answer_postprocess.py --input_path $FILE_PATH --save_path $SAVE_PATH/${FILE}_postprocess.jsonl
+    python3 preprocessing/instruction_generator.py --input_path $FILE_PATH --save_path $SAVE_PATH/${FILE}_instruction.jsonl --api_key $API_KEY
+    python3 preprocessing/postproc_question.py --input_path $SAVE_PATH/${FILE}_instruction.jsonl --save_path $SAVE_PATH/${FILE}_question.jsonl
+    python3 preprocessing/answer_generator.py --input_path $SAVE_PATH/${FILE}_question.jsonl --save_path $SAVE_PATH/${FILE}_answer.jsonl --api_key $API_KEY
+    python3 preprocessing/answer_postprocess.py --input_path $SAVE_PATH/${FILE}_answer.jsonl --save_path $SAVE_PATH/${FILE}_postprocess.jsonl
 done
