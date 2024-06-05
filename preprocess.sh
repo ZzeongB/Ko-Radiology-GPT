a950b0df4e2491fa4ab3e74a8fe0160ba2753ae6
ZzeongB <parkjeong02@snu.ac.kr>
2024-06-05 14:46:01 +0900

    fine-tuning code

--- preprocess.sh
+++ preprocess.sh
@@ -11,4 +11,6 @@ do
     python3 preprocessing/postproc_question.py --input_path $SAVE_PATH/${FILE}_instruction.jsonl --save_path $SAVE_PATH/${FILE}_question.jsonl
     python3 preprocessing/answer_generator.py --input_path $SAVE_PATH/${FILE}_question.jsonl --save_path $SAVE_PATH/${FILE}_answer.jsonl --api_key $API_KEY
     python3 preprocessing/answer_postprocess.py --input_path $SAVE_PATH/${FILE}_answer.jsonl --save_path $SAVE_PATH/${FILE}_postprocess.jsonl
+    python preprocessing/csv_to_jsonl_converter.py --input_path $SAVE_PATH/${FILE}_postprocess.jsonl --save_path $SAVE_PATH/${FILE}_final.jsonl
+
 done
